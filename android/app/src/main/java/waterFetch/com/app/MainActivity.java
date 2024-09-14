8f3f910487622e21a1024ce4daf43348b76ab454
ilyestearti5 <ilyestearti5@gmail.com>
2024-09-14 13:03:12 +0100

    Done

--- android/app/src/main/java/waterFetch/com/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package waterFetch.com.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 /// <reference types="vitest" />
-
-import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
+import pkg from "./package.json";
+import legacy from "@vitejs/plugin-legacy";
 import fs from "node:fs/promises";
 import { defineConfig } from "vite";
-import pkg from "./package.json";
-
 function incrementVersion(version: string) {
   // Check if the version has a ~ or ^ prefix
   const prefix = version[0] === "~" || version[0] === "^" ? version[0] : "";
   const versionWithoutPrefix = prefix ? version.slice(1) : version;
-
   const parts = versionWithoutPrefix.split(".").map(Number);
-
   if (prefix === "~") {
     // Increment the last part for ~ prefix
     parts[2]++;
@@ -30,23 +26,21 @@ function incrementVersion(version: string) {
       }
     }
   }
-
   return prefix + parts.join(".");
 }
-
 // https://vitejs.dev/config/
 export default defineConfig(async ({ command }) => {
   if (command == "build") {
     // update the version if it's in the build process
-    const newVersion = incrementVersion(pkg.version);
+    const version = incrementVersion(pkg.version);
     try {
       await fs.writeFile(
         "./package.json",
-        JSON.stringify({ ...pkg, version: newVersion }, undefined, 2),
+        JSON.stringify({ ...pkg, version }, undefined, 2),
         "utf-8"
       );
     } catch {}
-    console.log("Building For Version ...", newVersion);
+    console.log("Building For Version ...", version);
   }
   return {
     plugins: [react(), legacy()],
