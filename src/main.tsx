c88f19bcff5d4896037158e139bd40cf7a171e98
ilyestearti5 <ilyestearti5@gmail.com>
2024-10-04 22:52:17 +0100

    Refactor startApplication function to accept an options object

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import "water-fetch/ui/style.css";
 import { startApplication } from "water-fetch/ui/app";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
-startApplication(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+startApplication({
+  app: (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  ),
+  isDev: import.meta.env.DEV,
+});
