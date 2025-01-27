593e0fa4382f7bfb00ee9f696a5465a8a84758e5
Tiart Ilyes <ilyestearti5@gmail.com>
2025-01-27 21:39:58 +0100

    Refactor Electron app initialization and update package dependencies

--- electron/index.ts
+++ electron/index.ts
@@ -4,14 +4,11 @@ import {
   startApplicationForDesktop,
 } from "biqpod/electron";
 import { development, production } from "../app.json";
-
 setUpAppEnv({
   devUrl: "http://localhost:" + development.port,
   prodUrl: production.url,
 });
-
 export var mainWindow: Electron.BrowserWindow | null = null;
-
 startApplicationForDesktop(async () => {
   mainWindow = await createMainWindow();
 });
